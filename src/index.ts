e7646b4af3bebc3c2f1765bca740f420cccdfe67
Donato Jr <donato.junior.07@gmail.com>
2023-04-04 15:43:57 -0300

    feat: create discord connection

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-export default function helloWorld(name: string) {
-  return `Hello ${name}`
-}
+import { Events } from 'discord.js'
+import { config } from 'dotenv'
+import { client } from '@/discord/connection'
+import commandHandler from '@/discord/commandHandler'
+
+config()
+
+client.once(Events.ClientReady, () => {
+  console.log('Bot ready')
+})
+
+client.on(Events.MessageCreate, commandHandler)
+
+client.login(process.env.DISCORD_TOKEN)
