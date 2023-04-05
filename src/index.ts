85b35b5989302c0d272f3be8b6ad8f8be5404202
Donato Jr <donato.junior.07@gmail.com>
2023-04-05 19:18:26 -0300

    feat: app initialization

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import { Events } from 'discord.js'
 import { config } from 'dotenv'
-import { client } from '@/discord/connection'
+import { client } from '@/discord/client'
 import commandHandler from '@/discord/commandHandler'
 
-config()
+async function bootstrap() {
+  config()
 
-client.once(Events.ClientReady, () => {
-  console.log('Bot ready')
-})
+  client.once(Events.ClientReady, () => {
+    console.log('Bot ready')
+  })
 
-client.on(Events.MessageCreate, commandHandler)
+  client.on(Events.MessageCreate, commandHandler)
 
-client.login(process.env.DISCORD_TOKEN)
+  client.login(process.env.DISCORD_TOKEN)
+}
+
+bootstrap()
